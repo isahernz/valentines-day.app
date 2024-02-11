e2eb45dbf955c876300df04469cd91345fa37264
Brandon Isahir Hernández Camacho <isahernz@outlook.com>
2024-02-11 00:31:25 -0600

    style: :lipstick: update styles button component

--- src/components/DownloadButton.jsx
+++ src/components/DownloadButton.jsx
@@ -2,8 +2,8 @@ import DownloadIcon from "./icons/DownloadIcon.jsx";
 
 const DownloadButton = (props) => {
   return (
-    <button className="relative inline-flex h-12 overflow-hidden rounded-full p-[2px] max-w-max focus:outline-none focus:ring-2 focus:ring-gray-400 focus:ring-offset-2 focus:ring-offset-gray-50">
-      <span className="inline-flex gap-2 h-full w-full cursor-pointer items-center justify-center rounded-full bg-valentines-day-500 px-4 py-2 text-sm font-bold text-[#242422] backdrop-blur-3xl lg:text-base lg:px-6 lg:py-3">
+    <button className="relative inline-flex h-12 overflow-hidden rounded-full p-[2px] w-full sm:max-w-max focus:outline-none focus:ring-2 focus:ring-gray-400 focus:ring-offset-2 focus:ring-offset-gray-50">
+      <span className="inline-flex gap-2 h-full w-full cursor-pointer items-center justify-center rounded-full bg-valentines-day-500 px-8 py-3 text-sm font-bold text-valentines-day-900 backdrop-blur-3xl lg:text-base lg:px-6 lg:py-3">
         <DownloadIcon className={"size-6"} />
         {props.text}
       </span>
